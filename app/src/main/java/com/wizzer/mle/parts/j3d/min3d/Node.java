07b7beceec89c198df539bf86d0b1be00ef89150
Mark Millard <msm@wizzerworks.com>
2016-09-22 11:53:39 -0600

    Simplifying fragment shader to use a uniform color for the model.

--- app/src/main/java/com/wizzer/mle/parts/j3d/min3d/Node.java
+++ app/src/main/java/com/wizzer/mle/parts/j3d/min3d/Node.java
@@ -42,6 +42,8 @@ public class Node extends Object3dContainer
     // This will be used to pass in model color information.
     private int m_colorHandle;
 
+    float m_color[] = { 0.2f, 0.709803922f, 0.898039216f, 1.0f };
+
     public Node(I3dNodeTypeProperty.NodeType nodeType, I3dRole role)
     {
         super();
@@ -252,7 +254,7 @@ public class Node extends Object3dContainer
         // Set program handles. These will later be used to pass in values to the program.
         m_MVPMatrixHandle = GLES20.glGetUniformLocation(m_programHandle, "u_MVPMatrix");
         m_positionHandle = GLES20.glGetAttribLocation(m_programHandle, "a_Position");
-        m_colorHandle = GLES20.glGetAttribLocation(m_programHandle, "a_Color");
+        m_colorHandle = GLES20.glGetUniformLocation(m_programHandle, "v_Color");
 
         // Tell OpenGL to use this program when rendering.
         GLES20.glUseProgram(m_programHandle);
@@ -294,6 +296,7 @@ public class Node extends Object3dContainer
 
         GLES20.glEnableVertexAttribArray(m_positionHandle);
 
+        /*
         if (this.hasVertexColors())
         {
             // Pass in the color information
@@ -303,6 +306,10 @@ public class Node extends Object3dContainer
 
             GLES20.glEnableVertexAttribArray(m_colorHandle);
         }
+        */
+
+        // Set color for drawing the triangle
+        GLES20.glUniform4fv(m_colorHandle, 1, m_color, 0);
 
         // This multiplies the view matrix by the model matrix, and stores the result in the MVP
         // matrix (which currently contains model * view).
@@ -314,6 +321,9 @@ public class Node extends Object3dContainer
 
         GLES20.glUniformMatrix4fv(m_MVPMatrixHandle, 1, false, m_MVPMatrix, 0);
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
+
+        // Disable handle to the triangle vertices.
+        GLES20.glDisableVertexAttribArray(m_positionHandle);
     }
 
     public static void dump(IObject3dContainer objContainer)
@@ -343,14 +353,9 @@ public class Node extends Object3dContainer
         final String vertexShader =
               "uniform mat4 u_MVPMatrix;      \n"     // A constant representing the combined model/view/projection matrix.
             + "attribute vec4 a_Position;     \n"     // Per-vertex position information we will pass in.
-            + "attribute vec4 a_Color;        \n"     // Per-vertex color information we will pass in.
-
-            + "varying vec4 v_Color;          \n"     // This will be passed into the fragment shader.
 
             + "void main()                    \n"     // The entry point for our vertex shader.
             + "{                              \n"
-            + "   v_Color = a_Color;          \n"     // Pass the color through to the fragment shader.
-                                                      // It will be interpolated across the geometry.
             + "   gl_Position = u_MVPMatrix   \n"     // gl_Position is a special variable used to store the final position.
             + "               * a_Position;   \n"     // Multiply the vertex by the matrix to get the final point in
             + "}                              \n";    // normalized screen coordinates.
@@ -371,7 +376,7 @@ public class Node extends Object3dContainer
             GLES20.glGetShaderiv(vertexShaderHandle, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
 
             // If the compilation failed, delete the shader.
-            if (compileStatus[0] == 0)
+            if (compileStatus[0] == GLES20.GL_FALSE)
             {
                 GLES20.glDeleteShader(vertexShaderHandle);
                 vertexShaderHandle = 0;
@@ -393,14 +398,11 @@ public class Node extends Object3dContainer
         final String fragmentShader =
               "precision mediump float;       \n"     // Set the default precision to medium. We don't need as high of a
                                                       // precision in the fragment shader.
-            + "varying vec4 v_Color;          \n"     // This is the color from the vertex shader interpolated across the
-                                                      // triangle per fragment.
+            + "uniform vec4 v_Color;          \n"     // This is the color from the vertex shader.
             + "void main()                    \n"     // The entry point for our fragment shader.
             + "{                              \n"
             + "   gl_FragColor = v_Color;     \n"     // Pass the color directly through the pipeline.
             + "}                              \n";
-
-        boolean retValue = false;
 
         // Load in the fragment shader.
         int fragmentShaderHandle = GLES20.glCreateShader(GLES20.GL_FRAGMENT_SHADER);
@@ -418,7 +420,7 @@ public class Node extends Object3dContainer
             GLES20.glGetShaderiv(fragmentShaderHandle, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
 
             // If the compilation failed, delete the shader.
-            if (compileStatus[0] == 0)
+            if (compileStatus[0] == GLES20.GL_FALSE)
             {
                 GLES20.glDeleteShader(fragmentShaderHandle);
                 fragmentShaderHandle = 0;
@@ -455,7 +457,6 @@ public class Node extends Object3dContainer
 
             // Bind attributes
             GLES20.glBindAttribLocation(programHandle, 0, "a_Position");
-            GLES20.glBindAttribLocation(programHandle, 1, "a_Color");
 
             // Link the two shaders together into a program.
             GLES20.glLinkProgram(programHandle);
@@ -465,7 +466,7 @@ public class Node extends Object3dContainer
             GLES20.glGetProgramiv(programHandle, GLES20.GL_LINK_STATUS, linkStatus, 0);
 
             // If the link failed, delete the program.
-            if (linkStatus[0] == 0) {
+            if (linkStatus[0] == GLES20.GL_FALSE) {
                 GLES20.glDeleteProgram(programHandle);
                 programHandle = 0;
             }
@@ -473,7 +474,7 @@ public class Node extends Object3dContainer
 
         if (programHandle == 0)
         {
-            throw new MleRuntimeException("Error creating shaders.");
+            throw new MleRuntimeException("Error creating program for shaders.");
         }
 
         return programHandle;
